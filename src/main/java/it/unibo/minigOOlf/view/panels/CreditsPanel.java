cef19ce4bad0e0367cf5f7d8c655d7199333b335
Daniel <danielpatryk.bak@studio.unibo.it>
2026-03-21 20:25:19 +0100

    Removed the copy of GamePanel (the right one is in the .panels package). Added my UML schema to the notes folder.

--- src/main/java/it/unibo/minigOOlf/view/panels/CreditsPanel.java
+++ src/main/java/it/unibo/minigOOlf/view/panels/CreditsPanel.java
@@ -7,7 +7,7 @@ import java.awt.Dimension;
 /**
  * One of the possibile scenes, just a credits scene with the contribution of every 
  * component of the team :) 
- * TODO: it is a very optional class, do it only at the end 
+ * TODO: It is a very optional class, do it only at the end.
  */
 
 
