9a43d11b1809524f2e1831f7904dd0cb630a93d5
Giacomo Mengozzi <jackmeng2.7@gmail.com>
2026-03-22 16:06:11 +0100

    Added DynamicBody interface in model.physics

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.8")
 
     // Maven dependencies are composed by a group name, a name and a version, separated by colons
+    implementation("org.apache.commons:commons-math3:3.6.1")
     // implementation("com.omertron:API-OMDB:1.5")
     // implementation("org.jooq:jool:0.9.15")
 
---
+++ src/main/java/it/unibo/minigOOlf/model/physics/DynamicBody.java
@@ -0,0 +1,33 @@
+package it.unibo.minigOOlf.model.physics;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+/**
+ * Represents a dynamic physical body in 2D space with a position and velocity.
+ *
+ * <p>This interface is used by physics simulation components to read and update
+ * the motion state of moving objects.</p>
+ */
+public interface DynamicBody {
+
+    /**
+     * Returns the current position of the body in 2D space.
+     *
+     * @return the position as a {@link Vector2D}
+     */
+    Vector2D getPosition();
+
+    /**
+     * Returns the current velocity of the body in 2D space.
+     *
+     * @return the velocity as a {@link Vector2D}
+     */
+    Vector2D getVelocity();
+
+    /**
+     * Updates the body's velocity.
+     *
+     * @param velocity the new velocity as a {@link Vector2D}
+     */
+    void setVelocity(Vector2D velocity);
+}
