4f93e1783fe611b9d81b0b471accf9f0bb9cec50
Daniel <danielpatryk.bak@studio.unibo.it>
2026-04-03 15:31:27 +0200

    Added in .gitignore: DS_Store (mac os specific folder navigation info, useless)

--- src/main/java/it/unibo/minigoolf/App.java
+++ src/main/java/it/unibo/minigoolf/App.java
@@ -2,11 +2,11 @@ package it.unibo.minigoolf;
 
 import it.unibo.minigoolf.controller.MainControllerImpl;
 
-
+/**
+ * Entry point from where the game starts.
+ */
 public class App {
     public static void main(String[] args) {
         new MainControllerImpl().start();
-
     }
 }
-
