9d29382d499cc7fd4cc890c852119aac3463d237
Giacomo Mengozzi <giacomo.mengozzi4@studio.unibo.it>
2026-05-08 21:47:23 +0200

    Finished model.map.factory.SecondMapFactory

--- src/main/java/it/unibo/minigoolf/model/map/factories/SecondMapFactory.java
+++ src/main/java/it/unibo/minigoolf/model/map/factories/SecondMapFactory.java
@@ -3,7 +3,6 @@ package it.unibo.minigoolf.model.map.factories;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
-
 import it.unibo.minigoolf.util.Vector2D;
 
 import it.unibo.minigoolf.model.ball.BallImpl;
@@ -64,8 +63,57 @@ public class SecondMapFactory implements GameMapFactory {
     private static final double GRASS4_WIDTH = 270;
     private static final double GRASS4_HEIGHT = 1080;
     private static final int GRASS4_Z_INDEX = 0;
+    private static final double ICE_X = 1240;
+    private static final double ICE_Y = 270;
+    private static final double ICE_WIDTH = 410;
+    private static final double ICE_HEIGHT = 540;
+    private static final int ICE_Z_INDEX = 4;
+
+    private static final double O1_X = 525;
+    private static final double O1_Y = 15;
+    private static final double O1_WIDTH = 50;
+    private static final double O1_HEIGHT = 700;
+    private static final double O2_X = 800;
+    private static final double O2_Y = 565;
+    private static final double O2_WIDTH = 50;
+    private static final double O2_HEIGHT = 500;
+    private static final double O3_X = 1000;
+    private static final double O3_Y = 15;
+    private static final double O3_WIDTH = 50;
+    private static final double O3_HEIGHT = 300;
+    private static final double O4_X = 1215;
+    private static final double O4_Y = 245;
+    private static final double O4_WIDTH = 50;
+    private static final double O4_HEIGHT = 590;
+    private static final double O5_X = 1625;
+    private static final double O5_Y = 245;
+    private static final double O5_WIDTH = 50;
+    private static final double O5_HEIGHT = 590;
+    private static final double O6_X = 1230;
+    private static final double O6_Y = 245;
+    private static final double O6_WIDTH = 410;
+    private static final double O6_HEIGHT = 50;
+    private static final double O7_X = 1230;
+    private static final double O7_Y = 785;
+    private static final double O7_WIDTH = 410;
+    private static final double O7_HEIGHT = 50;
+    private static final double O8_X = 275;
+    private static final double O8_Y = 400;
+    private static final double O8_RADIUS = 100;
+    private static final Vector2D O9_V1 = new Vector2D(850, 15);
+    private static final Vector2D O9_V2 = new Vector2D(1000, 15);
+    private static final Vector2D O9_V3 = new Vector2D(1000, 165);
+    private static final Vector2D O10_V1 = new Vector2D(850, 1065);
+    private static final Vector2D O10_V2 = new Vector2D(1100, 1065);
+    private static final Vector2D O10_V3 = new Vector2D(850, 915);
+    private static final double O11_X = 550;
+    private static final double O11_Y = 715;
+    private static final double O11_RADIUS = 25;
+
     private static final double BALL_RADIUS = 30;
     private static final Vector2D BALL_INITIAL_POSITION = new Vector2D(80, 80);
+    private static final double HOLE_RADIUS = 40;
+    private static final Vector2D HOLE_POSITION = new Vector2D(1770, 520);
 
     /**
      * Builds a simple test game map.
@@ -100,6 +148,10 @@ public class SecondMapFactory implements GameMapFactory {
                         SAND_HEIGHT, Color.WHITE),
                 SurfaceType.SAND.getFriction(), SAND_Z_INDEX, SurfaceType.SAND.getTexturePath()));
         surfaces.add(new RectangularSurface(
+                new Rectangle(new Vector2D(ICE_X, ICE_Y), ICE_WIDTH,
+                        ICE_HEIGHT, Color.WHITE),
+                SurfaceType.ICE.getFriction(), ICE_Z_INDEX, SurfaceType.ICE.getTexturePath()));
+        surfaces.add(new RectangularSurface(
                 new Rectangle(new Vector2D(DIRT_X, DIRT_Y), DIRT_WIDTH,
                         DIRT_HEIGHT, Color.WHITE),
                 SurfaceType.DIRT.getFriction(), DIRT_Z_INDEX, SurfaceType.DIRT.getTexturePath()));
@@ -107,9 +159,17 @@ public class SecondMapFactory implements GameMapFactory {
         obstacles.add(new WallObstacle(new Vector2D(0, 0), 1920, 15, Color.BLACK));
         obstacles.add(new WallObstacle(new Vector2D(1905, 0), 15, 1080, Color.BLACK));
         obstacles.add(new WallObstacle(new Vector2D(0, 1065), 1920, 15, Color.BLACK));
-        obstacles.add(new WallObstacle(new Vector2D(300, 200), 40, 100, Color.ORANGE));
-        obstacles.add(new RoundObstacle(new Vector2D(700, 200), 50, Color.ORANGE));
-        obstacles.add(new TriangleObstacle(new Vector2D(900, 400), new Vector2D(900, 450), new Vector2D(750, 400), Color.ORANGE));
-        return new GameMapImpl(surfaces, new BallImpl(BALL_INITIAL_POSITION, BALL_RADIUS), obstacles, new HoleImpl(new Vector2D(1500, 800), 40));
+        obstacles.add(new WallObstacle(new Vector2D(O1_X, O1_Y), O1_WIDTH, O1_HEIGHT, Color.darkGray));    
+        obstacles.add(new WallObstacle(new Vector2D(O2_X, O2_Y), O2_WIDTH, O2_HEIGHT, Color.darkGray));    
+        obstacles.add(new WallObstacle(new Vector2D(O3_X, O3_Y), O3_WIDTH, O3_HEIGHT, Color.darkGray));    
+        obstacles.add(new WallObstacle(new Vector2D(O4_X, O4_Y), O4_WIDTH, O4_HEIGHT, Color.darkGray));    
+        obstacles.add(new WallObstacle(new Vector2D(O5_X, O5_Y), O5_WIDTH, O5_HEIGHT, Color.darkGray));    
+        obstacles.add(new WallObstacle(new Vector2D(O6_X, O6_Y), O6_WIDTH, O6_HEIGHT, Color.darkGray));    
+        obstacles.add(new WallObstacle(new Vector2D(O7_X, O7_Y), O7_WIDTH, O7_HEIGHT, Color.darkGray));    
+        obstacles.add(new RoundObstacle(new Vector2D(O8_X, O8_Y), O8_RADIUS, Color.darkGray));
+        obstacles.add(new TriangleObstacle(O9_V1, O9_V2, O9_V3, Color.darkGray));
+        obstacles.add(new TriangleObstacle(O10_V1, O10_V2, O10_V3, Color.darkGray));
+        obstacles.add(new RoundObstacle(new Vector2D(O11_X, O11_Y), O11_RADIUS, Color.darkGray));
+        return new GameMapImpl(surfaces, new BallImpl(BALL_INITIAL_POSITION, BALL_RADIUS), obstacles, new HoleImpl(HOLE_POSITION, HOLE_RADIUS));
     }
 }
