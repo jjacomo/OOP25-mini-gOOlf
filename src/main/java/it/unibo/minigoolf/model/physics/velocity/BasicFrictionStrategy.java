80e60c8c8a3514712be576411dab5b1b3c4e6859
Giacomo Mengozzi <giacomo.mengozzi4@studio.unibo.it>
2026-05-11 21:33:33 +0200

    update todays report

--- src/main/java/it/unibo/minigoolf/model/physics/velocity/BasicFrictionStrategy.java
+++ src/main/java/it/unibo/minigoolf/model/physics/velocity/BasicFrictionStrategy.java
@@ -37,5 +37,39 @@ public class BasicFrictionStrategy implements BallVelocityStrategy {
         }
     }
 
+    // da mettere quando non verra' piu' modificata la velocita' in MainControllerImpl
+
+    // @Override
+    // public void updateVelocity(Ball ball, Surface surface, double deltaTime) {
+    //     final Vector2D velocity = ball.getVelocity();
+    //     final double velocityNorm = velocity.getNorm();
+    //     final double surfaceFriction = surface.getFriction();
+    //     double friction = 0;
+
+    //     if(velocityNorm == 0) {
+    //         return;
+    //     }
+    //     if (velocityNorm > 600) {
+    //         // Attrito dinamico più forte a velocità molto elevate, per evitare che la palla diventi incontrollabile.
+    //         friction = 15000 * surfaceFriction; // per test, da tarare meglio
+    //         LOGGER.debug("High speed: Velocity norm: {}, surface friction: {}, deltaTime: {}", velocityNorm, surfaceFriction, deltaTime);
+    //     } else if (velocityNorm > 50) {
+    //         // Attrito diretto dalla superficie e modulato dalla velocità:
+    //         // basso quando la palla è veloce, crescente man mano che rallenta.
+    //         friction = 10000 * surfaceFriction / velocityNorm; // per test, da tarare meglio
+    //         LOGGER.debug("Velocity norm: {}, surface friction: {}, deltaTime: {}", velocityNorm, surfaceFriction, deltaTime);
+    //     } else if (velocityNorm > 5) {
+    //         // Attrito più forte quando la palla è lenta, per evitare che si trascini troppo.
+    //         friction = 500 * surfaceFriction; // per test, da tarare meglio
+    //         LOGGER.debug("Low speed: Velocity norm: {}, surface friction: {}, deltaTime: {}", velocityNorm, surfaceFriction, deltaTime);
+    //     } else {
+    //         LOGGER.debug("Very low speed: setting velocity to zero.");
+    //         ball.setVelocity(new Vector2D(0, 0));
+    //     }
+
+    //     final Vector2D frictionForce = velocity.normalize().scalarMultiply(-friction);
+    //     final Vector2D newVelocity = velocity.add(frictionForce.scalarMultiply(deltaTime));
+    //     ball.setVelocity(newVelocity);
+    // }
 
 }
