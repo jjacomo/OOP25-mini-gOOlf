c17314776ca9a24342a680e3771df95c0d568b41
Mattia <mattia.dambrosio5@studio.unibo.it>
2026-06-22 16:25:53 +0200

    Added JavaDoc comment in ObstacleData

--- src/main/java/it/unibo/minigoolf/controller/obstaclecontroller/ObstacleData.java
+++ src/main/java/it/unibo/minigoolf/controller/obstaclecontroller/ObstacleData.java
@@ -4,6 +4,10 @@ import it.unibo.minigoolf.util.shapes.Shape;
 
 /**
  * Data Transfer Object containing visual information about an obstacle.
+ * 
+ * @param shape the shape of the obstacle
+ * @param bounciness the bounciness of the obstacle
+ * @param isPortal true if the obstacle is a portal, false otherwise
  */
 public record ObstacleData(Shape shape, double bounciness, boolean isPortal) {
 }
